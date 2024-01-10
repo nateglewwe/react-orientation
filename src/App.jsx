376dd47b640cd2bfb96ad3c4e345cf92b13df5f1
Nathaniel Glewwe <nateglewwe@gmail.com>
2024-01-09 21:06:27 -0600

    Partway through adding a global state, re-watch end of Ja n 9th video to finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState} from 'react';
 import './App.css'
 
 import Footer from './components/Footer/Footer';
@@ -7,10 +6,19 @@ import Card from './components/Card/Card';
 import Header from './components/Header/Header';
 
 function App() {
+  const [globalName, setGlobalName] = useState();
+
+  const clickGlobalNameChange = () =>{
+    setGlobalName('GLOBAL!');
+  } 
   return (
     <>
     {/* Header Component*/}
       <Header />
+
+      <button></button>
+
+      <Card />
       <Card />
       <Footer />
     </>
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,9 +1,25 @@
+//To use state, we need hooks
+import {useState} from 'react';
+
 function Card () {
+    const [myName, setMyName] = useState('Nathaniel');
+
+    //props.globalName
+
+    function clickChangeName () {
+        //set name to new name
+        console.log('Testing!');
+        setMyName('NATHANIEL');
+    }
+
     return (
         <div className="card">
         <button>
           count is 0
         </button>
+        <p>{props.globalName ? props.globalName : myName}</p>
+        <p>{myName}</p>
+        <button onClick={clickChangeName}>Change Name</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
